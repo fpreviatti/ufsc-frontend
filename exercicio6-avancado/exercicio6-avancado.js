90d13a0e5b3cc73e2fa9e03c34f88bea0c9e0c84
Fabio <fpreviatti@hotmail.com>
2022-10-25 18:20:19 -0300

    exercicio 6 quase pronto

--- exercicio6-avancado/exercicio6-avancado.js
+++ exercicio6-avancado/exercicio6-avancado.js
@@ -1,10 +1,18 @@
+
 const element = document.getElementById("botao-login");
 element.addEventListener("click", inicio);
 
-USUARIOS_KEY = 'usuarios';
+const element2 = document.getElementById("botao-criar");
+element2.addEventListener("click", cadastro);
 
-var usuarios;
-var usuario;
+const element3 = document.getElementById("botao-cadastro");
+element3.addEventListener("click", criarUsuario);
+
+
+const USUARIOS_KEY = 'usuarios';
+
+var usuarios = [];
+var usuario = '';
 
 function initializeWebStorage(){
 
@@ -17,13 +25,12 @@ function initializeWebStorage(){
   }
 }
 
+
 function inicio(){
 
   initializeWebStorage();
 
-  //login();
-
-  cadastro();
+  login();
 
 }
 
@@ -37,24 +44,54 @@ class Usuario {
 
 }
 
-function cadastro(){
+
+function criarUsuario(){
+
+  initializeWebStorage();
 
   let emailRecuperado = document.getElementById("email").value;
   let senhaRecuperada = document.getElementById("senha").value;
 
   this.usuario = new Usuario(emailRecuperado,senhaRecuperada);
-  this.usuarios = JSON.parse(localStorage.getItem(USUARIOS_KEY));
 
   if(isExist(emailRecuperado)){
-    alert('Já existe este e-mail cadastrado');
+    alert('Usuário já cadastrado');
   }
+
 
   else{
+    this.usuarios = JSON.parse(localStorage.getItem(USUARIOS_KEY));
 
-    this.usuarios.push(usuario);
+    this.usuarios.push(this.usuario);
+  
     localStorage.setItem(USUARIOS_KEY, JSON.stringify(this.usuarios));
-
   }
+
+  
+}
+
+function cadastro(){
+
+  var titulo = document.getElementById("span-titulo");
+
+  titulo.innerHTML = 'Faça seu cadastro!';
+
+  element.style = 'display: none';
+
+  element3.style = 'display: inline';
+
+
+  var spanConta = document.getElementById('span-conta');
+
+  spanConta.innerHTML = 'Já possui conta? Faça seu login!';
+
+  var botao = document.getElementById('botao-criar');
+
+  botao.innerHTML = 'Login';
+
+  botao.addEventListener("click", function(){
+    document.location.href = 'exercicio6-avancado.html';
+});
 
 }
 
@@ -79,17 +116,29 @@ function login(){
 
   }
 
-  //alert('Bem vindo!');
-
   var resposta = document.getElementById("div-login-id");
   resposta.innerHTML = '';
+
+  var tituloBemVindo = document.createElement("h2");
+
+  var hrefDeslogar = document.createElement("a");
+
+  hrefDeslogar.href = '/exercicio6-avancado/exercicio6-avancado.html';
+
+  hrefDeslogar.innerHTML = "Deslogar";
+
+  tituloBemVindo.innerHTML = 'Bem Vindo : ' +this.usuario.email;
+
+  document.body.appendChild(tituloBemVindo);
+
+  document.body.appendChild(hrefDeslogar);
 
 }
 
 function buscaUsuario(email){
   
   for (let u of this.usuarios) {
-    if (u.email?.valueOf() == email?.valueOf()) {
+    if (u.email.valueOf() == email.valueOf()) {
       return u;
     }
   }
@@ -106,9 +155,3 @@ function isExist(email){
   return false;
 }
 
-function usuariosCadastrados(){
-
-    this.usuarios = localStorage.get(USUARIOS_KEY);
-    return this.usuarios;
-
-}
